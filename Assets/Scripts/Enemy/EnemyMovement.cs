566f3d266e52acbbe19b2bcda6a32d76be1264f5
William Darian <williamdariansutedjo@gmail.com>
2022-10-03 14:02:35 +0700

    Enemy Movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(LineRenderer))]
+public class EnemyMovement : MonoBehaviour {
+
+  public EnemyPathwayState State { get; set; }
+
+  [field: SerializeField]
+  public float Speed { get; set; } = 1;
+
+  [field: SerializeField]
+  public Rigidbody2D MovingBody;
+
+  private LineRenderer line;
+
+  public void Awake() {
+    Assert.IsNotNull(this.MovingBody);
+    this.line = this.GetComponent<LineRenderer>();
+  }
+
+  public void Start() {
+    this.State = new EnemyPathwayState();
+  }
+
+  public void Update() {
+    Vector3 nextPoint = this.line.transform.position
+      + this.line.GetPosition(this.State.NextPointIndex);
+    Vector2 position = this.MovingBody.transform.position;
+    Vector2 delta = (Vector2) nextPoint - position;
+    this.MovingBody.velocity = this.Speed * delta.normalized;
+
+    if (delta.magnitude < 0.01f * this.Speed) {
+      this.State.NextPointIndex += 1;
+      this.State.NextPointIndex %= this.line.positionCount;
+
+      return;
+    }
+  }
+}
---
+++ Assets/Scripts/Enemy/EnemyPathwayState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class EnemyPathwayState {
+
+  public Vector2 Position = Vector2.zero;
+  public Vector2 Velocity = Vector2.zero;
+  public Int32 NextPointIndex = 0;
+}
+
